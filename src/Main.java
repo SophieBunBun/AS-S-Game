2b892286d882dddeb410867606e6008b42037e09
SophieBunBun <sophie.bun.lullaby@gmail.com>
2025-06-25 23:08:31 +0100

    Sophie 1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import data.Lang;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean loop = true;
+        Scanner scan = new Scanner(System.in);
+
+        while (loop){
+            System.out.println(Lang.mainScreenTXT);
+            String in = scan.nextLine();
+
+        }
+    }
+}
---
+++ src/data/Lang.java
@@ -0,0 +1,6 @@
+package data;
+
+public class Lang {
+
+    public static String mainScreenTXT = "1 - Shop\n2 - Expeditions\n3 - Green house\n4 - Laboratory\n5 - Clients\n";
+}
